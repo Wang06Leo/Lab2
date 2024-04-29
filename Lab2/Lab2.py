20b50c86055637d0138c46d8c14198f36db28a1d
wangrongyi <WANG06.23@iChat.sp.edu.sg>
2024-04-29 11:09:38 +0800

    new

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -5,7 +5,8 @@ def main():
         max_num = max(numbers)
         min_num = min(numbers)
         average_num = calculate_average(numbers)
-        display_results(average_num, max_num, min_num)
+        median_num = calc_median_number(numbers)
+        display_results(average_num, max_num, min_num, median_num)
    else:
         print("No numbers input")
 
@@ -24,12 +25,24 @@ def get_num():
 
 def calculate_average(numbers):
     return sum(numbers) / len(numbers)
+def calc_median_number(numbers):
+    sorted_numbers = sorted(numbers)
+    length = len(sorted_numbers)
+    if length % 2 == 0:
+        # If the length is even, calculate the average of the middle two numbers
+        middle_index = length // 2
+        median = (sorted_numbers[middle_index - 1] + sorted_numbers[middle_index]) / 2
+    else:
+        # If the length is odd, take the middle number
+        median = sorted_numbers[length // 2]
+    return median
 
-def display_results(average_num, max_num, min_num):
+def display_results(average_num, max_num, min_num, median_num):
     print("\nTemperature Analysis Results:")
     print(f"Average Number: {average_num:.2f}")
     print(f"Minimum Number: {min_num}")
     print(f"Maximum Number: {max_num}")
+    print(f"Median Number: {median_num}")
 
 if __name__ == "__main__":
     main()
--- bmi/bmi.py
+++ bmi/bmi.py
@@ -5,9 +5,11 @@ def calculate_bmi(height, weight):
  print("BMI = " + str(bmi))
  if bmi< 18.5:
   print("Under Weight")
+  return -1
  if bmi> 25:
   print("Over Weight")
+  return 1
  else :
   print("normal weight")
+  return 0
 calculate_bmi(weight=57, height=1.73)
-#做改变用的
